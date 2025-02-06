6b4c5a93ac4baf68f9dbf4d628a44def2aa51dfa
KarlGudsoon <maturana.or.adrian@gmail.com>
2025-02-06 17:26:21 -0300

    Update tippy.js

--- Borradores/template-articulo/tippy.js
+++ Borradores/template-articulo/tippy.js
@@ -1,80 +1,14 @@
-tippy('#cerebro', {
-  content: "Crecimiento del cerebro",
-  theme: "red",
-});
-
-tippy('#bipedismo', {
-  content: "Bipedismo y posición erguida",
-  theme: "red",
-});
-
-tippy('#conversacion', {
-  content: "Lenguaje simbólico",
-  theme: "red",
-});
-
-tippy('#hielobering', {
-  content: "Zona bajo hielo",
-  theme: "red",
-});
-
-tippy('#nacionalidad-sapiens', {
-  content: "Todo el mundo",
-  theme: "red",
-});
-
-tippy('#logro-sapiens', {
-  content: "-Agricultura <br> -Domesticación del Ganado <br> -Pensamiento abstracto",
-  theme: "red",
-});
-
-tippy('#fecha-sapiens', {
-  content: "Apareció hace 120.000 años atras, el humano moderno pertenece a este especie humana",
-  theme: "red",
-});
-
-tippy('#draggable-1', {
-  content: "Los cazadores del Paleolítico utilizaban una gran variedad de técnicas de caza para sobrevivir, tales como la emboscada, la persecución y el uso de trampas.",
-  theme: "red",
-});
-
-tippy('#glaciacion-scratch', {
-  content: "La glaciación de Würm, se puede observar en la pelicula 'Era de Hielo', en la cual aparecen un grupo de humanos que se adaptan al frío extremo",
-  theme: "red",
-  followCursor: true,
-});
-
-tippy('#lenguaje', {
-  content: "La glaciación de Würm, se puede observar en la pelicula 'Era de Hielo', en la cual aparecen un grupo de humanos que se adaptan al frío extremo",
-  theme: "red",
-});
-
-tippy('#ritualidad', {
-  content: "La glaciación de Würm, se puede observar en la pelicula 'Era de Hielo', en la cual aparecen un grupo de humanos que se adaptan al frío extremo",
-  theme: "red",
-});
-
-tippy('#arte', {
-  content: "La glaciación de Würm, se puede observar en la pelicula 'Era de Hielo', en la cual aparecen un grupo de humanos que se adaptan al frío extremo",
-  theme: "red",
-});
-
-tippy('#img-etapas', {
-  content: "Este proceso y los nuevos avances tecnológicos cambiaron la forma de vida de las personas y su relación con el entorno. Dependiendo del contexto, el cambio desde el trabajo manual al trabajo con maquinas fue progresivo o radical.",
-  theme: "black",
-  followCursor: true,
-});
-
 tippy('#desv-siderurgica', {
   content: "En esta industria no existía mucha seguridad para los obreros, por ello, no era raro encontrarse con accidentes en la frabríca.",
   theme: "red",
+  trigger: 'mouseenter focus click',
   
 });
 
 tippy('#ven-siderurgica', {
   content: "En esta industria no existía mucha seguridad para los obreros, por ello, no era raro encontrarse con accidentes en la frabríca.",
   theme: "green",
-  
+  trigger: 'mouseenter focus click',
 });
 
 
