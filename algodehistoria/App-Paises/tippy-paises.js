f9f1a849631bb3546e270863c517380ea2219279
KarlGudsoon <maturana.or.adrian@gmail.com>
2026-07-09 18:31:58 -0400

    renovación 09/07/2026

--- algodehistoria/App-Paises/tippy-paises.js
+++
@@ -1,4 +0,0 @@
-tippy('#poblacion-chile', {
-  content: "19.49 millones (2021)",
-  theme: "red",
-});
--- algodehistoria/Apps/tareas/crear_tarea.php
+++
@@ -1,6 +0,0 @@
-<form action="guardar_tarea.php" method="POST">
-    <input type="text" name="titulo" placeholder="Título de la tarea" required>
-    <textarea name="descripcion" placeholder="Descripción de la tarea" required></textarea>
-    <input type="date" name="fecha_limite" required>
-    <button type="submit">Crear Tarea</button>
-</form>
